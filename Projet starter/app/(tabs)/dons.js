0fe174826fd92ebe29a82a664d28e22d3be0e1f8
maximegii <maxime.girardet@ecole-isitech.fr>
2024-06-04 17:22:14 +0200

    creation du projet authente modification du projet association

--- Projet starter/app/(tabs)/dons.js
+++ Projet starter/app/(tabs)/dons.js
@@ -1,18 +1,72 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ActivityIndicator, ScrollView, Image } from "react-native";
+import { WebView } from 'react-native-webview';
+import React from "react";
+
+
 
 
 export default function DonsScreen() {
+
+  const [loading, setLoading] = React.useState(true);
+  const handleWebViewLoad = () => {
+    setLoading(false);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Faites un don à notre association pour soutenir toutes nos actions dans le monde</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image source={require("../../assets/splash.png")} style={styles.image} />
+      {loading && (
+        <View style={styles.spinner}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )}
+      <WebView style={{ flex: 1, height: 400 }} source={{ uri: "https://react-paypal-js-storybook.fly.dev/buttons.html" }} onLoad={handleWebViewLoad} />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+  },
+  TextTete: {
+    fontSize: 25,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  image: {
+    width: 109,
+    height: 100,
+    marginBottom: 20,
+    marginTop: 20,
+    alignSelf: "center",
+  },
+  textpetit: {
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  TextDon: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+  },
+  spinner: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    zIndex: 1,
   },
 });
--- Projet starter/app/(tabs)/index.js
+++ Projet starter/app/(tabs)/index.js
@@ -7,6 +7,7 @@ export default function HomeScreen() {
       <Image 
       style={styles.img}
       source={require('./../../assets/splash.png')} />
+      <Text>Bienvenue sur sauver des vies, l’association humanitaire qui œuvre pour un monde meilleur. Depuis notre création en 2009, nous rassemblons des bénévoles dévoués qui souhaitent donner de leur temps et de leur énergie pour aider les plus démunis. Nous croyons que les défis de notre société nécessitent une réflexion et une action concertées. C’est pourquoi nous encourageons chaque individu, homme ou femme, jeune ou moins jeune, à prendre en charge son destin et à contribuer à la construction d’un monde équilibré et solidaire. Car nous sommes convaincus que sauver une vie, c’est sauver l’humanité toute entière…</Text>
 
     </View>
   );
